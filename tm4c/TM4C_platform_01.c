3f0a6640b8fe6d4f5af3506409c48384a048a5c0
Lukasz Janyst <lukasz@jany.st>
2016-04-25 11:14:05 +0200

    TM4C01: Consolidate platform 01 into one file

--- tm4c/TM4C_platform_01.c
+++ tm4c/TM4C_platform_01.c
@@ -17,8 +17,17 @@
 // along with silly-invaders.  If not, see <http://www.gnu.org/licenses/>.
 //------------------------------------------------------------------------------
 
+#define __IO_IMPL__
+
 #include <io/IO.h>
+#include <io/IO_device.h>
+#include <io/IO_error.h>
+#include <io/IO_display.h>
+#include <io/IO_display_low.h>
+
 #include "TM4C.h"
+
+#include <drivers/pcd8544/pcd8544.h>
 
 //------------------------------------------------------------------------------
 // Initialize the platform
@@ -26,4 +35,104 @@
 int32_t IO_init()
 {
   return TM4C_init();
+}
+
+//------------------------------------------------------------------------------
+// Initialize a button
+//------------------------------------------------------------------------------
+int32_t IO_button_init(IO_io *io, uint8_t module, uint16_t flags)
+{
+  if(module > 1)
+    return -IO_EINVAL;
+
+  if(module == 0)
+    return IO_gpio_init(io, 40, flags, 0);
+
+  return IO_gpio_init(io, 44, flags, 0);
+}
+
+//------------------------------------------------------------------------------
+// Initialize an LED
+//------------------------------------------------------------------------------
+int32_t IO_led_init(IO_io *io, uint8_t module)
+{
+  if(module > 0)
+    return -IO_EINVAL;
+
+  return IO_gpio_init(io, 37, 0, 1);
+}
+
+//------------------------------------------------------------------------------
+// The display structs
+//------------------------------------------------------------------------------
+static pcd8544 display0;
+
+//------------------------------------------------------------------------------
+// Write the pixel matrix to the device
+//------------------------------------------------------------------------------
+static int32_t display_sync(IO_io *io)
+{
+  return PCD8544_sync(&display0);
+}
+
+//------------------------------------------------------------------------------
+// Initialize a display device
+//------------------------------------------------------------------------------
+int32_t IO_display_init_low(IO_io *io, uint8_t module)
+{
+  if(module != 0)
+    return -IO_EINVAL;
+
+  //----------------------------------------------------------------------------
+  // Initialize the display 0
+  //----------------------------------------------------------------------------
+  io->type    = IO_DISPLAY;
+  io->sync    = display_sync;
+  io->channel = 0;
+  io->flags   = 0;
+  io->read    = 0;
+  io->write   = 0;
+  io->event   = 0;
+  return PCD8544_init(&display0, 0, 6, 7);
+}
+
+//------------------------------------------------------------------------------
+// Get parameters of the device
+//------------------------------------------------------------------------------
+int32_t IO_display_get_attrs_low(IO_io *io, IO_display_attrs *attrs)
+{
+  if(io->type != IO_DISPLAY || io->channel != 0)
+    return -IO_EINVAL;
+
+  return PCD8544_get_attrs(attrs);
+}
+
+//------------------------------------------------------------------------------
+// Clear the display
+//------------------------------------------------------------------------------
+int32_t IO_display_clear_low(IO_io *io)
+{
+  if(io->type != IO_DISPLAY || io->channel != 0)
+    return -IO_EINVAL;
+
+  return PCD8544_clear(&display0);
+}
+
+//------------------------------------------------------------------------------
+// Put a pixel on the screen
+//------------------------------------------------------------------------------
+int32_t IO_display_put_pixel(IO_io *io, uint16_t x, uint16_t y, uint32_t argb)
+{
+  if(io->type != IO_DISPLAY || io->channel != 0)
+    return -IO_EINVAL;
+
+  return PCD8544_put_pixel(&display0, x, y, argb);
+}
+
+//------------------------------------------------------------------------------
+// Get number of display devices available
+//------------------------------------------------------------------------------
+int32_t IO_display_count_low()
+{
+  return 1;
 }
